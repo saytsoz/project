e61449b988b714fca11de156188c6dab2fc83bd6
Farhod <webusta@inbox.ru>
2020-07-17 12:58:29 +0500

    fitrst commit

---
+++ js/script.js
@@ -0,0 +1,125 @@
+"use strict";
+
+// type of var
+// number
+// let number = 4.6;
+// console.log(-4/0);
+// console.log('string' * 4);
+
+// string
+// const persone = "Alex";
+
+// Boolean
+
+// const bool = false;
+
+// console.log(something);
+
+// let  und;
+
+// console.log(und);
+// alert(und);
+
+
+// object 
+
+
+// const  obj = {
+//     name: 'jone',
+//     age: 25,
+//     isMarried: false, 
+// };
+
+// console.log(obj.age);
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 6, false, [], {}];
+// console.log(arr[1]);
+
+// const answer = confirm('Are you here?');
+// console.log(answer);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+
+
+// !!! все информация от пользователя приходят как строк
+
+
+// const answers = [];
+
+// answers[0] = prompt('Как тебя зовут', '');
+// answers[1] = prompt('Как тебя фамилия', '');
+// answers[2] = prompt('Сколькл вам лет', '');
+
+// document.write(answers);
+
+// канкатанация
+
+// const category = 'toys';
+
+// console.log('https://demo.uz/' + category + '/4');
+// console.log(`https://demo.uz/${category}/4`);
+
+// let user = 'Ivan';
+
+// alert(`Привет ${user}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
