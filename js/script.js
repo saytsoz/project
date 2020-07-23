767ec2df0cd25fe1f60d9c76700cc2e441df6424
Farhod <webusta@inbox.ru>
2020-07-23 10:07:39 +0500

    mod

--- js/script.js
+++ js/script.js
@@ -144,6 +144,177 @@
 //         break;
 // }
 
+// let num = 50;
+
+// while (num < 55){
+//     console.log(num);
+//     num++;
+// }
+//  do{
+//     console.log(num);
+//     num++;
+//  }
+//  while(num < 55);
+
+
+// for(let i = 1; i <= 8; i++){
+//     if(i === 6){
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// 2 - практика
+
+
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+// const personalMoveDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// };
+
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//       b = +prompt("На сколько оцените его?", ""),
+//       c = prompt("Один из последних просмотренных фильмов?", ""),
+//       d = +prompt("На сколько оцените его?", "");
+
+// personalMoveDB.movies[a] = b;
+// personalMoveDB.movies[c] = d;
+
+
+// for(let i = 0; i < 2; i++){
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//           b = +prompt("На сколько оцените его?", "");
+
+//     if(a != null && b != null && a != '' && b != '' && a.length < 50){
+//         personalMoveDB.movies[a] = b;
+//         console.log(done);
+//     }else{
+//         console.log('error');
+//         i--;
+//     }
+
+          
+// }
+
+// console.log(personalMoveDB);
+
+// if(personalMoveDB.count < 10){
+//     console.log("Просмотрено довольно мало фильмов");
+// }else if(personalMoveDB.count >= 10 && personalMoveDB.count <= 30){
+//     console.log("Вы классический зритель");
+// }else if(personalMoveDB.count > 30){
+//     console.log("Вы киноман");
+// }else{
+//     console.log("Произошла ошибка");
+// }
+
+
+// const farmDb = {
+//     sheep: {},
+//     cow:{}
+// }
+
+// for(let i = 0; i < 2; i++){
+    
+//     const a = prompt('cinde of sheep', ''),
+//           b = prompt('count of sheep', '');
+//     if(a != null && b != null && a != '' && b != '' && a.length < 50){
+//         farmDb.sheep[a] = b;
+//         console.log('done');
+//     }else{
+//         console.log('Error');
+//         i--;
+//     }
+    
+// }
+
+
+// console.log(farmDb);
+
+
+
+// function
+let num = 20;
+
+function showFirstMessage(text){
+    console.log(text);
+    let num = 10;
+    console.log(num);
+}
+
+showFirstMessage('hello world');
+console.log(num);
+
+
+function calc(a, b){
+    return (a + b);
+}
+
+console.log(calc(4, 5));
+console.log(calc(5, 6));
+console.log(calc(8, 6));
+
+function ret(){
+    let num = 50;
+
+
+    return num;
+}
+
+const anotherNum = ret();
+
+console.log(anotherNum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
